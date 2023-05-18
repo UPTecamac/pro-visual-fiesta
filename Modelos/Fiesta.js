e5580d282048c6a89979122569d60f6d773b917b
Emmanuel <emmanuel_torres@uptecamac.edu.mx>
2023-05-18 11:52:59 -0600

    Imagenes dinamicas y creación de personas

--- Modelos/Fiesta.js
+++ Modelos/Fiesta.js
@@ -4,6 +4,9 @@ class Fiesta{
     boletos = 0
     cantidadRecolectada = 0;
     precioEntrada = 100
+    resolucionX = 0
+    resolucionY = 0
+    coordenadasPersonas = []
 
     // -- Variables deconfiguracion y control
     shotBienvenidaHombre = 2
@@ -12,20 +15,25 @@ class Fiesta{
     shotBienvenidaMenor28 = 3
     shotBienvenidaMayor28 = 1
 
+    constructor()
+    {
+        this.obtenerResolucion()
+    }
+
     registrarPersona (nombre, apellido, edad, sexo)
     {
         if(edad >= 18){            
             // -- Verificar si es hombre o mujer para cobrar la cuota de entrada
             this.verificarCuotaEntrada(sexo)
-            let persona = new Persona(nombre, apellido, edad, sexo)
-            this.iniciarBienvenida(persona)
+            let persona = new Persona(nombre, apellido, edad, sexo, this.personasFiesta.length + 1, this.resolucionX, this.resolucionY)
+            this.iniciarBienvenida(persona)            
             this.personasFiesta.push(persona)
-            this.boletos++
-            this.crearGifPersona(sexo)
+            this.boletos++            
         }
         else
         {
             console.log('No se puede registrar a la persona, es menor de edad')
+            alert('No se puede registrar a la persona, es menor de edad')
         }            
     }
 
@@ -60,7 +68,7 @@ class Fiesta{
 
     iniciarBienvenida(persona){
         // -- shot de bienvenida por sexo
-        console.log("%c¡Bienvenido! "+persona.nombre,"background:linear-gradient(#000, #555); color:#fff; padding: 5px 10px;");
+        //console.log("%c¡Bienvenido! "+persona.nombre,"background:linear-gradient(#000, #555); color:#fff; padding: 5px 10px;");
         if(persona.sexo == true)
         {            
             persona.tomarAlcohol(this.shotBienvenidaHombre)
@@ -70,7 +78,7 @@ class Fiesta{
             persona.tomarAlcohol(this.shotBienvenidaMujer)
         }
         // -- shot de bienvenida por edad
-        persona.tomarAlcohol(this.verificarShotBienvenidaEdad(persona.edad))
+        //persona.tomarAlcohol(this.verificarShotBienvenidaEdad(persona.edad))
     }
 
     // -- Metodo para verificar los shot de bienvenida por edad
@@ -130,36 +138,15 @@ class Fiesta{
         return personas
     }
 
-    crearGifPersona(sexo)
+    // -- Método para obtener resolucion de pantalla
+    obtenerResolucion()
     {
-        // -- insertar imagen en body
-        let img = document.createElement('img') 
-        if(sexo){
-            img.src = './img/person/man_1.gif'
-            img.style.width = '150px'
-            img.style.height = '150px'
-        }            
-        else
-        {
-            img.src = './img/person/woman_1.gif'
-            img.style.width = '100px'
-            img.style.height = '100px'
-        }                    
-        img.style.position = 'absolute'
-        let posicion = this.posicionAleatoria()
-        img.style.top = posicion[0] + 'px'
-        img.style.left = posicion[1] + 'px'
-                
-        document.body.appendChild(img)
+        this.resolucionX = window.innerWidth
+        this.resolucionY = window.innerHeight        
     }
 
+    // -- Método par|a obtener x, y sin salirse de la resolucion
+    
 
-    // -- Método para obtener x, y aleatorios a la resolución de la pantalla
-    posicionAleatoria()
-    {
-        let x = Math.floor(Math.random() * (window.innerWidth - 100))
-        let y = Math.floor(Math.random() * (window.innerHeight - 100))
-        return [x, y]
-    }
 
 }
--- Modelos/Persona.js
+++ Modelos/Persona.js
@@ -7,6 +7,10 @@ class Persona
     limiteAlcohol = 0;
     estadoAnimo = 'Tranquilo'
     actividad = ''
+    id = 0
+    imagen = ''
+    resolucionX = 0
+    resolucionY = 0
 
     Actividades = {
         Bailar : 'Bailar',
@@ -16,11 +20,15 @@ class Persona
         Jugar : 'Jugar',
     }
 
-    constructor(name, surname, age, sex) {
+    constructor(name, surname, age, sex, id, resolucionX, resolucionY) {
         this.nombre = name
         this.apellido = surname
         this.edad = age
         this.sexo = sex
+        this.id = id
+        this.resolucionX = resolucionX
+        this.resolucionY = resolucionY
+        this.crearGifPersona()
     }
 
     verificarAlcohol() {
@@ -30,6 +38,8 @@ class Persona
             this.estadoAnimo = 'Feliz'
         else
             this.estadoAnimo = 'Borracho'
+
+        this.verificarImagenPersona()
     }
 
     tomarAlcohol(nShots)
@@ -62,4 +72,116 @@ class Persona
         }
     }
 
+    crearGifPersona(x, y)
+    {
+        let persona = this
+        let posicion = this.posicionAleatoria()
+        x = posicion[0]
+        y = posicion[1]
+
+        // -- crear div para imagen y label
+        let div = document.createElement('div')
+        div.style.position = 'absolute'
+        div.style.top = x + 'px'
+        div.style.left = y + 'px'
+        div.style.width = '150px'
+        div.style.height = '150px'
+        div.style.display = 'flex'
+        div.style.flexDirection = 'column'
+        div.style.justifyContent = 'center'
+        div.style.alignItems = 'center'
+        div.style.textAlign = 'center'
+        div.style.color = 'White'
+        div.style.fontSize = '18px'
+        div.style.fontFamily = 'Comic Sans MS'
+        div.style.zIndex = '1'
+        div.id = 'div_' + this.id
+        div.className = 'personas'
+        div.onclick = function () {
+            persona.informacionPersona()
+            persona.acciones()
+        }                        
+        document.body.appendChild(div)
+        // -- crear label para insertar en div
+        let label = document.createElement('label')
+        label.id = 'label_' + this.id
+        label.innerHTML = this.nombre + ' ' + this.apellido
+        div.appendChild(label)
+
+        let labelEstado = document.createElement('label')
+        labelEstado.id = 'label_estado' + this.id
+        labelEstado.innerHTML = this.estadoAnimo
+        labelEstado.style.fontSize = '12px'        
+        div.appendChild(labelEstado)
+
+        // -- insertar imagen en Label
+        let img = document.createElement('img') 
+        if(this.sexo){
+            img.src = './img/person/man_1.gif'
+            img.style.width = '150px'
+            img.style.height = '150px'
+        }            
+        else
+        {
+            img.src = './img/person/woman_1.gif'
+            img.style.width = '100px'
+            img.style.height = '100px'
+        }                            
+        img.id = 'person_' + this.id
+        this.imagen = img.id       
+        div.appendChild(img)        
+    }
+
+        // -- Método para verificar imagen de persona por estado de animo
+    verificarImagenPersona()
+    {        
+        let img = document.getElementById(this.imagen)       
+        let label = document.getElementById('label_estado' + this.id)  
+        label.innerHTML = this.estadoAnimo        
+        if(this.estadoAnimo == 'Tranquilo')
+        {            
+            if(this.sexo)
+            {
+                img.src = './img/person/man_1.gif'
+            }
+            else
+            {
+                img.src = './img/person/woman_1.gif'
+            }
+        }
+        else if(this.estadoAnimo == 'Feliz')
+        {
+            img.src = './img/person/feliz.gif'
+        }
+        else
+        {
+            img.src = './img/person/borracho.gif'
+        }
+    }
+
+    posicionAleatoria()
+    {
+        let posicion = []
+        posicion[0] = Math.floor(Math.random() * (this.resolucionY - 150))
+        posicion[1] = Math.floor(Math.random() * (this.resolucionX - 150))
+        return posicion        
+    }
+
+    // -- Método para obtener resolucion de pantalla
+    obtenerResolucion()
+    {
+        this.resolucionX = window.innerWidth
+        this.resolucionY = window.innerHeight        
+    }
+
+    informacionPersona(){
+        console.log(this)
+    }
+
+    // -- Metodo para saber que quiere hacer la persona
+    acciones()
+    {
+        // -- tomar
+        this.tomarAlcohol(1)
+    }
 }
